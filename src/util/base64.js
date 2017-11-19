9d2088992a5b895fc816b8fb8bf66d41c618642c
Sam Gammon <sam@momentum.io>
2017-11-19 09:54:25 -0800

    Add base64 tools

---
+++ src/util/base64.js
@@ -0,0 +1,86 @@
+
+/**
+ * Bloombox Utils: Base64
+ *
+ * @fileoverview Provides tools for encoding and decoding data with Base64.
+ */
+
+/*global goog */
+
+goog.provide('bloombox.util.b64');
+
+goog.require('goog.crypt.base64');
+
+
+/**
+ * Base64 encoding function that takes string data, and base64 encodes it. By
+ * default, this function will not append `=` values (value 64, meaning
+ * `nothing`), because the matching implementation below (`decode`) used to
+ * convert information back uses the same beahvior.
+ *
+ * Bloombox services (server-side) will also properly handle these strings. For
+ * perfect interop with other systems, pass `true` for the second parameter,
+ * `opt_dont_clean`.
+ *
+ * @param {string} data String to encode as base64.
+ * @param {boolean=} opt_dont_clean Optionally, do not remove `=` from the
+ *        output before returning. This flag prevents any modification of the
+ *        underlying return value at all.
+ * @return {string} Base64-encoded version of `data`.
+ */
+bloombox.util.b64.encode = function(data, opt_dont_clean) {
+  let encoded = goog.crypt.base64.encodeString(data, false);
+  if (!opt_dont_clean) return encoded;
+  return encoded.replace(/=/g, '');
+};
+
+
+/**
+ * Base64 decoding function that takes a string, encoded as base64, and decodes
+ * it. By default, this function will append two `=` values (value 64, meaning
+ * `nothing`, if an equals sign is not present at the end of the string),
+ * because ranges that are not properly padded (according to the spec) will
+ * properly decode in that circumstance, since the underlying engine will
+ * normally discard extra `nothing` markers past the padding boundary.
+ *
+ * The matching encoder implementation presented by this module (`encode`) uses
+ * the same behavior presented here - i.e. it omits `=` values. If you want
+ * interoperability with non-Bloombox systems, pass `true` to `opt_dont_append`,
+ * but this is generally a safer operation then omitting them during encoding
+ * and can safely be left off all the time.
+ *
+ * @param {string} data Base64-encoded string to decode.
+ * @param {boolean=} opt_dont_append Optionally, do not append anything to the
+ *        end of the string to account for missing padding. This flag prevents
+ *        any modification of the input value at all.
+ * @return {string} String, after being decoded through Base64.
+ */
+bloombox.util.b64.decode = function(data, opt_dont_append) {
+  let data = !opt_dont_append ? (
+    !data.endsWith('=') ? `${data}==` : data) : data;
+  return goog.crypt.base64.decodeString(data, false);
+};
+
+
+/**
+ * Encode a string using Base64, but with the "URL-safe" variant provided by
+ * Google and used extensively internally by them.
+ *
+ * @param {string} data Data to encode as a websafe B64 string.
+ * @return {string} Websafe base64-encoded string.
+ */
+bloombox.util.b64.encodeWebsafe = function(data) {
+  return goog.crypt.base64.encodeString(data, true);
+};
+
+
+/**
+ * Decode a string that is encoded in Base64, but using the "URL-safe" variant
+ * of the alphabet.
+ *
+ * @param {string} data Web-safe base64-encoded string.
+ * @return {string} Decoded string.
+ */
+bloombox.util.b64.decodeWebsafe = function(data) {
+  return goog.crypt.base64.decodeString(data, true);
+};
