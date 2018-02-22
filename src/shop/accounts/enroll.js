2ee8fcad3fee7466f43bde96528520abdb970bd2
Sam Gammon <sam@momentum.io>
2018-02-21 16:26:48 -0800

    Trim channel before sending for enrollment event

--- src/shop/accounts/enroll.js
+++ src/shop/accounts/enroll.js
@@ -292,9 +292,9 @@ bloombox.shop.enroll.Enrollment.prototype.sendAnalytics = function(key,
     bloombox.telemetry.InternalCollection.ENROLLMENT,
     {'action': opt_error ? 'error' : 'enroll',
      'status': opt_status ? opt_status : 200,
-     'customer': {'foreignId': this.foreignId},
+     'customer': {'foreignId': this.foreignId.trim()},
      'enrollment': {
-        'key': this.key,
+        'key': this.key.trim(),
         'channel': this.channel,
         'source': this.source}}).send();
 };
