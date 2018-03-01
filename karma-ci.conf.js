3605c333cb674e6ba93e9f20a9824e2a65978770
Sam Gammon <sam@momentum.io>
2018-02-28 22:00:12 -0800

    Don't test in Phantom

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -380,8 +380,6 @@ module.exports = function(config) {
     autoWatch: false,
     customLaunchers: Object.assign({}, browserstackProfiles, saucelabsProfiles),
     browsers: [
-      'PhantomJS',
-
       // BrowserStack
       //'firefox_mac_latest', 'chrome_mac_latest', 'safari_mac_latest'
       //'firefox_mac_oldest', 'chrome_mac_oldest', 'safari_mac_oldest',
