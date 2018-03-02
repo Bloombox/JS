076adb7c9890c9688b6bbe403cbb0f796472e33e
Sam Gammon <sam@momentum.io>
2018-03-02 14:03:07 -0800

    Use correct Babel env

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
 
     babelPreprocessor: {
       options: {
-        presets: ['env'],
+        presets: ['@babel/env'],
         sourceMap: 'inline'
       },
       filename: function (file) {
