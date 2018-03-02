58a8117ab3e2d88a7d185c15767a6acc654c8ce9
Sam Gammon <sam@momentum.io>
2018-03-02 10:34:20 -0800

    Run Safari tests locally

--- karma.conf.js
+++ karma.conf.js
@@ -111,6 +111,7 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
      //'PhantomJS',
+     'Safari',
      'ChromeCanary'],
 
 
