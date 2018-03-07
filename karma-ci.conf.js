8cb690cd3117a6faeb65fae78fbaffc587ac27b5
Sam Gammon <sam@momentum.io>
2018-03-06 19:10:23 -0800

    Add Safari

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -547,8 +547,8 @@ module.exports = function(config) {
     browsers: Array.from(new Set([
       // BrowserStack
       'firefox_mac_latest',
-      'chrome_mac_latest'
-      //'safari_mac_latest',
+      'chrome_mac_latest',
+      'safari_mac_latest',
       //'firefox_mac_oldest', 'chrome_mac_oldest', 'safari_mac_oldest',
       //'firefox_windows_latest', 'chrome_windows_latest', 'edge_16',
       //'ie_11', 'firefox_windows_oldest', 'chrome_windows_oldest',
@@ -559,7 +559,7 @@ module.exports = function(config) {
       //  return item.name;
       //})))),
     singleRun: true,
-    concurrency: 1,
+    concurrency: 2,
     tunnelIdentifier: process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
     startTunnel: false,
     startConnect: false,
