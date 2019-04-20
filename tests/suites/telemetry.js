19240d2f17c8adb12f1dc30f1e543373a6fd02fa
Sam Gammon <sam@bloombox.io>
2019-04-20 16:21:40 -0700

    Add 'ping' to event telemetry testsuite

--- tests/suites/telemetry.js
+++ tests/suites/telemetry.js
@@ -5,12 +5,26 @@ function genTelemetryTestsuite(version) {
   const apiOpts = version === 'v0' ? {} : {'beta': true};
 
   describe('service: telemetry (' + version + ')', function() {
-    describe('telemetry: analytics event ingest', function() {
+    describe('method: `ping`', function() {
+      it('should be able to complete a ping', function() {
+        return new Promise((resolve, reject) => {
+          return bloombox.telemetry.events(apiOpts).ping((latency) => {
+              if (latency > -1) {
+                resolve(latency);
+              } else {
+                reject(latency);
+              }
+            });
+        });
+      });
+    });
+
+    describe('method: `event`', function() {
       it('should be able to send a basic test event', function() {
         const collection = bloombox.telemetry.Collection
           .named('testsuite');
 
-        bloombox.telemetry.events(apiOpts)
+        return bloombox.telemetry.events(apiOpts)
           .event(collection, {'test': 'data', 'goes': 'here'});
       });
     });
