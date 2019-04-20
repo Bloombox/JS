183d1ce6e4cb06d56fe7592265df07418b68e15a
Sam Gammon <sam@bloombox.io>
2019-04-20 16:01:40 -0700

    Fix telemetry testsuite (working v0)

--- tests/suites/telemetry.js
+++ tests/suites/telemetry.js
@@ -1,13 +1,23 @@
 /* eslint-env mocha */
 'use strict';
 
-function telemetryTestsuite() {
-  describe('service: telemetry', function() {
+function genTelemetryTestsuite(version) {
+  const apiOpts = version === 'v0' ? {} : {'beta': true};
+
+  describe('service: telemetry (' + version + ')', function() {
     describe('telemetry: analytics event ingest', function() {
       it('should be able to send a basic test event', function() {
-        bloombox.telemetry.event('testsuite', {'test': 'data', 'goes': 'here'})
-          .send();
+        const collection = bloombox.telemetry.Collection
+          .named('testsuite');
+
+        bloombox.telemetry.events(apiOpts)
+          .event(collection, {'test': 'data', 'goes': 'here'});
       });
     });
   });
+}
+
+function telemetryTestsuite() {
+  genTelemetryTestsuite('v0');
+  // genTelemetryTestsuite('v1');
 }
